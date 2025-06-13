71ce6ad743a94bcae9e8d139d5ad1fddd6544fef
ReixeXD <anderson-silva2003@hotmail.com>
2025-06-13 13:13:12 -0300

    first commit

---
+++ src/test/java/com/reize/StudyTrack/StudyTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reize.StudyTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
